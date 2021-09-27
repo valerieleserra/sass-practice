231088ff8c5717586288522b664801d9c0882b8c
valerieleserra <valerieleserra@gmail.com>
2021-09-27 16:22:12 -0400

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="nabar">
+      <a href="/home">Home</a>
+      <a href="/about">About</a>
+      <a href="/profile">Profile</a>
+      <a href="/contact">Contact</a>
+    </div>
+  )
+}
